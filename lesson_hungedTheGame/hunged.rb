f7d24bf6249f030c4bd021299a54465f15c70256
Kole565 <rjkz565rjkz565@gmail.com>
2021-06-16 12:47:19 +0300

    Testing branch init. new implement method use in main file

--- lesson_hungedTheGame/hunged.rb
+++ lesson_hungedTheGame/hunged.rb
@@ -19,8 +19,20 @@ while (errors < attempts)
 	puts("Ask letter")
 	
 	choice = get_user_input()
+	choice_check = check_letter(goal_array, good_letters, bad_letters, choice)
+	
+	if (choice_check == 1)
+		good_letters << choice
+		update_screen(good_letters, bad_letters)
+	
+	elsif (choice_check == 0)
+		# We just mercy if player choice same letter
 
-	check_letter(goal_array, good_letters, bad_letters, choice, errors)
+	else
+		errors += 1
+		bad_letters << choice
+		update_screen(good_letters, bad_letters)
+	end
 
 end
 
