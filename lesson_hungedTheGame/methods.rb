803b986812bd4d3a3b6734d919c2750510f59696
Kole565 <rjkz565rjkz565@gmail.com>
2021-06-16 12:30:34 +0300

    Create methods user input and lettter check

---
+++ lesson_hungedTheGame/methods.rb
@@ -0,0 +1,37 @@
+puts("Methods loading start")
+
+def get_user_input()
+	inp = nil
+	
+	while (inp == nil || inp.to_i != 0)
+		inp = STDIN.gets.encode("UTF-8").chomp
+	end
+
+	return inp
+end
+
+def check_letter(goal_array, good_letters, bad_letters, choice)
+	res = nil
+
+	if (goal_array.include?(choice))
+
+		if ((good_letters + bad_letters).include?(choice))
+			res = 0
+		else
+			res = 1
+		end
+
+	else 
+		
+		if ((good_letters + bad_letters).include?(choice))
+			res = 0
+		else
+			res = -1
+		end
+
+	end
+
+	return res
+end
+
+puts("Methods loaded")
