986f02cec4b4cfde9e792457f333c9a2db389a9f
Kole565 <rjkz565rjkz565@gmail.com>
2021-06-16 13:15:27 +0300

    Testing continue

--- lesson_hungedTheGame/hunged.rb
+++ lesson_hungedTheGame/hunged.rb
@@ -16,26 +16,26 @@ attempts = 7
 # Game
 puts("Game start")
 
-# while (errors < attempts)
-# 	puts("Ask letter")
+while (errors < attempts)
+	puts("Ask letter")
 	 
-# 	choice = get_user_input()
-# 	choice_check = check_letter(goal_array, good_letters, bad_letters, choice)
+	choice = get_user_input()
+	choice_check = check_letter(goal_array, good_letters, bad_letters, choice)
 	
-# 	if (choice_check == 1)
-# 		good_letters << choice
-# 		update_screen(good_letters, bad_letters)
+	if (choice_check == 1)
+		good_letters << choice
+		update_screen(good_letters, bad_letters)
 	
-# 	elsif (choice_check == 0)
-# 		# We just mercy if player choice same letter
+	elsif (choice_check == 0)
+		# We just mercy if player choice same letter
 
-# 	else
-# 		errors += 1
-# 		bad_letters << choice
-# 		update_screen(good_letters, bad_letters)
-# 	end
+	else
+		errors += 1
+		bad_letters << choice
+		update_screen(good_letters, bad_letters)
+	end
 
-# end
+end
 
 
 pust("Game end")
