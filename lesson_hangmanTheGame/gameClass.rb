5dfe11661c5229b3697cfd1f7703b2f24a0be5b8
Kole565 <rjkz565rjkz565@gmail.com>
2021-06-18 16:02:28 +0300

    Game class add

---
+++ lesson_hangmanTheGame/gameClass.rb
@@ -0,0 +1,66 @@
+class Game
+
+	def initialize(goal_word)
+		@letters = get_letters(goal_word)
+		@good_letters = []
+		@bad_letters = []
+		@errors = 0
+		@status = 0
+	end
+
+	def get_value(value)
+		return instance_variable_get("@#{value}")
+	end
+	
+	def get_letters(goal_word)
+		
+		if (goal_word == nil || goal_word == "")
+			abort ("Целевое слово не указано")
+		else
+			return goal_word.split("")
+		end
+
+	end
+
+	def ask_next_letter()
+		puts("Введите следующую букву: ")
+
+		inp = ""
+		while inp == ""
+			inp = STDIN.gets.encode("UTF-8").chomp
+		end
+
+		next_step(inp)
+	end
+
+	def next_step(letter)
+		
+		if (@status == -1 || @status == 1)
+			return
+
+		elsif (@good_letters.include?(letter) || @bad_letters.include?(letter))
+			return
+		
+		end
+
+		if (@letters.include?(letter))
+			@good_letters << letter
+
+			if (@good_letters.uniq.sort == @letters.uniq.sort)
+				@status = 1
+			end
+
+		else
+			@bad_letters << letter
+
+			if (errors >= 7)
+				@status = -1
+			end
+
+		end
+
+	end
+end
+
+game = Game.new("жираф")
+puts(game.get_value("letters").to_s)
