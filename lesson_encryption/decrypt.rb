7fa9a070fcb4e798687b19d0b20d53f87884bbe3
Kole565 <rjkz565rjkz565@gmail.com>
2021-07-05 17:13:39 +0300

    Encryption lesson insight created.

---
+++ lesson_encryption/decrypt.rb
@@ -0,0 +1,3 @@
+# Decryption progaramm part
+# That's programm shoould ask user hash,
+# encryption method and return decrypted value (string)
---
+++ lesson_encryption/encrypt.rb
@@ -0,0 +1,3 @@
+# Encryption progaramm part
+# That's programm shoould ask user string,
+# encryption method and return encrypted value (hash)
