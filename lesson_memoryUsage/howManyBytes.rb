e20787778f256df768281a82d48e708b0e9f954d
Kole565 <rjkz565rjkz565@gmail.com>
2021-07-06 10:55:15 +0300

    Add file whose check defined var or not.

--- lesson_memoryUsage/howManyBytes.rb
+++ lesson_memoryUsage/howManyBytes.rb
@@ -1,5 +1,3 @@
-require "objspace"
-
 what_to_count = [
 42,
 "Василий",
---
+++ lesson_memoryUsage/varIsDefined.rb
@@ -0,0 +1,16 @@
+require "objspace"
+
+a = 42
+# b = "Hi"
+
+if (defined?(a))
+	puts("Var a exist")
+else
+	puts("Var a don't exist")
+end
+
+if (defined?(b))
+	puts("Var b exist")
+else
+	puts("Var b don't exist")
+end
