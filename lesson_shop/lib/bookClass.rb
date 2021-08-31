841ade71417108bddb739362c59aa21af5c34583
Kole565 <rjkz565rjkz565@gmail.com>
2021-08-31 08:38:08 +0300

    Release book, film and product class in shop

--- lesson_shop/lib/bookClass.rb
+++ lesson_shop/lib/bookClass.rb
@@ -1,5 +1,44 @@
 require_relative("productClass.rb")
+require_relative("methods.rb")
 
 class Book < Product
+	
+	def initialize(cost, stock, name, genre, author)
+		# Call parent constructor
+		super(cost, stock)
+
+		# Add special var's
+		@name = name.downcase
+		@genre = genre.downcase
+		@author = author.downcase
+	end
+
+	def name
+		return @name.capitalize
+	end
+
+	def genre
+		return @genre
+	end
+
+	def author
+		return @author.split.map(&:capitalize)*' '
+	end
+
+	def to_s
+		return "Book <<#{name}>>, #{genre}, author - #{author}, #{cost} rub. (stock #{stock})"
+	end
+
+	def self.from_file(file_path)
+		text = get_lines_from_file(file_path)
+
+		text.each_with_index do | attribute, index |
+			if attribute.empty?
+				text[index] = "Nan"
+			end
+		end
+
+		self.new(text[3], text[4], text[0], text[1], text[2])
+	end
 
 end
--- lesson_shop/lib/productClass.rb
+++ lesson_shop/lib/productClass.rb
@@ -1,17 +1,41 @@
 class Product
 
-	def initialize(cost, amount)
+	def initialize(cost, stock)
 		# Var's init
-		@cost = cost
-		@amount = amount
+		@cost = cost.to_f
+		@stock = stock.to_i
+	end
+
+	def to_s
+		# Abstract class
 	end
 
 	def cost
 		return @cost
 	end
 
-	def amount
-		return @amount
+	def stock
+		return @stock
+	end
+
+	def update(**kwargs)
+		if kwargs
+			
+			for key, value in kwargs
+				puts(value)
+				set_value(key, value)
+			end
+
+		end
+
+	end
+
+	def set_value(value_name, value_param)
+		instance_variable_set("@#{value_name.downcase}", value_param)
+	end
+
+	def self.from_file(*args)
+		raise NotImplementedError
 	end
 
 end
