238ac64f6be25f44f2c7fb7e301a3c81c3c87c19
Kole565 <rjkz565rjkz565@gmail.com>
2021-09-18 19:47:21 +0300

    Deleate frozen projects

--- project_waterIsLive/settings.rb
+++
@@ -1,2 +0,0 @@
-account_standarts = ["name", "gender", "age", "weight"]        # Whats's fields have every Account's class exemplar
-calc_params = [0.04, 0.6, 0.03, 0.4]                           # Calc init values
